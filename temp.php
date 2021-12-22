bde676c73278162e7eb30575fdd089a43df6d2d8
Tam <75450151+thicctaccc@users.noreply.github.com>
2021-12-22 21:06:54 +0100

    Final Exam

---
+++ temp.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+
+</head>
+<body>
+<?php
+
+$val = $_GET["val"];
+
+$res= (($val - 32)) * 5/9;
+
+echo $res;
+
+?>
